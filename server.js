00bda0f8c77fb25966fbf4fdf68e6e1bb7edb415
bigdaddyjean <jeannappoleon@pursuit.org>
2023-02-02 13:06:10 -0500

    backend completed

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // DEPENDENCIES
+require("dotenv").config();
+const app = require("./app")
 
 // CONFIGURATION
 
-
 // LISTEN
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`app is listening on port ${PORT}`))
